e9dc493a932f912cee71e3cb7f295c2938356ed1
SS <62755890+SagarSundriyal@users.noreply.github.com>
2026-02-23 19:01:13 +0530

    Delete main directory

--- main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE orders (
-    order_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    product_id BIGINT,
-    product_name VARCHAR(255),
-    quantity INT,
-    status VARCHAR(50),
-    order_date TIMESTAMP
-);
